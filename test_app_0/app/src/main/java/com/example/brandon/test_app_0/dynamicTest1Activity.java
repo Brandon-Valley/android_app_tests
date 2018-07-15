d2a6c0727393d1813b636fc2ef766ee84eea828f
Brandon Valley <bavvh8@mst.edu>
2018-07-15 16:57:05 -0500

    added dynamic button

--- test_app_0/app/src/main/java/com/example/brandon/test_app_0/dynamicTest1Activity.java
+++ test_app_0/app/src/main/java/com/example/brandon/test_app_0/dynamicTest1Activity.java
@@ -3,6 +3,8 @@ package com.example.brandon.test_app_0;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class dynamicTest1Activity extends AppCompatActivity {
@@ -22,6 +24,18 @@ public class dynamicTest1Activity extends AppCompatActivity {
 
         //TextView textView = findViewById(R.id.textView3);
         //textView.setText(message);
+
+
+
+
+
+        Button b2=new Button(this);
+        b2.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+        b2.setText("Dynamic");
+        b2.setTextSize(30);
+        //b2.setTextColor(b1.getTextColors());
+        LinearLayout container = (LinearLayout)findViewById(R.id.linear_layout_0);
+        container.addView(b2);
 
 
 
