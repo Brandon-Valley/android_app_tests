f87645e45976868d06f5bc0dbc94d61ab4804d07
Brandon Valley <bavvh8@mst.edu>
2018-07-15 18:57:17 -0500

    dynamically added card

--- test_app_0/app/src/main/java/com/example/brandon/test_app_0/dynamicTest1Activity.java
+++ test_app_0/app/src/main/java/com/example/brandon/test_app_0/dynamicTest1Activity.java
@@ -1,8 +1,12 @@
 package com.example.brandon.test_app_0;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.util.TypedValue;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -34,9 +38,53 @@ public class dynamicTest1Activity extends AppCompatActivity {
         b2.setText("Dynamic");
         b2.setTextSize(30);
         //b2.setTextColor(b1.getTextColors());
+
+
         LinearLayout container = (LinearLayout)findViewById(R.id.linear_layout_0);
         container.addView(b2);
 
+
+
+        // Get the application context
+        Context mContext = getApplicationContext();
+
+        // Initialize a new CardView
+        CardView card = new CardView(mContext);
+
+        // Set the CardView layoutParams
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+        card.setLayoutParams(params);
+
+        // Set CardView corner radius
+        card.setRadius(9);
+
+        // Set cardView content padding
+        card.setContentPadding(15, 15, 15, 15);
+
+        // Set a background color for CardView
+        card.setCardBackgroundColor(Color.parseColor("#FFC6D6C3"));
+
+        // Set the CardView maximum elevation
+        card.setMaxCardElevation(15);
+
+        // Set CardView elevation
+        card.setCardElevation(9);
+
+        // Initialize a new TextView to put in CardView
+        TextView tv = new TextView(mContext);
+        tv.setLayoutParams(params);
+        tv.setText("CardView\nProgrammatically");
+        tv.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 30);
+        tv.setTextColor(Color.RED);
+
+        // Put the TextView in CardView
+        card.addView(tv);
+
+        // Finally, add the CardView in root layout
+        container.addView(card);
 
 
     }
