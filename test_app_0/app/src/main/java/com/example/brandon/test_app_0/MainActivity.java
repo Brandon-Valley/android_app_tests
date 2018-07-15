a02f1032049aef5363090d1d2ca7ef10b776fefb
Brandon Valley <bavvh8@mst.edu>
2018-07-15 15:59:07 -0500

    added card test activity

--- test_app_0/app/src/main/java/com/example/brandon/test_app_0/MainActivity.java
+++ test_app_0/app/src/main/java/com/example/brandon/test_app_0/MainActivity.java
@@ -12,6 +12,7 @@ import static java.sql.DriverManager.println;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.brandon.test_app_0.MESSAGE";
+    public static final String EXTRA_MESSAGE_2 = "com.example.brandon.test_app_0.MESSAGE_2";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +26,16 @@ public class MainActivity extends AppCompatActivity {
         String message = editText.getText().toString();
 //        println(message);
         intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    /** Called when the user taps the go to card activity button */
+    public void goToCardActivity(View view) {
+        Intent intent = new Intent(this, CardTestActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText2);
+        String message = editText.getText().toString();
+//        println(message);
+        intent.putExtra(EXTRA_MESSAGE_2, message);
         startActivity(intent);
     }
 
