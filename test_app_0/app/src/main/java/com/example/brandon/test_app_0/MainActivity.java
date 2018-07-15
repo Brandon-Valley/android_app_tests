dbf5ed29b9e0809a032253e081114050a82beb88
Brandon Valley <bavvh8@mst.edu>
2018-07-15 16:41:16 -0500

    added dynamic test 1 activity

--- test_app_0/app/src/main/java/com/example/brandon/test_app_0/MainActivity.java
+++ test_app_0/app/src/main/java/com/example/brandon/test_app_0/MainActivity.java
@@ -13,6 +13,8 @@ import static java.sql.DriverManager.println;
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.brandon.test_app_0.MESSAGE";
     public static final String EXTRA_MESSAGE_2 = "com.example.brandon.test_app_0.MESSAGE_2";
+    public static final int EXTRA_INT_0 = 5;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +34,18 @@ public class MainActivity extends AppCompatActivity {
     /** Called when the user taps the go to card activity button */
     public void goToCardActivity(View view) {
         Intent intent = new Intent(this, CardTestActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editTextNumCards);
+        String message = editText.getText().toString();
+//        println(message);
+        intent.putExtra(EXTRA_MESSAGE_2, message);
+        startActivity(intent);
+    }
+
+
+
+    /** Called when the user taps the go to card activity button */
+    public void goToDynamicTest1Activity(View view) {
+        Intent intent = new Intent(this, dynamicTest1Activity.class);
         EditText editText = (EditText) findViewById(R.id.editTextNumCards);
         String message = editText.getText().toString();
 //        println(message);
