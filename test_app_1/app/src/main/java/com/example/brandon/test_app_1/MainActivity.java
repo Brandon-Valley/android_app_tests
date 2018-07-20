0f953b4fd9e0f4dfccc8322d69b683e3ed1d23a3
Brandon Valley <bavvh8@mst.edu>
2018-07-19 22:46:44 -0500

    fixed first project, just needed to clean

--- test_app_1/app/src/main/java/com/example/brandon/test_app_1/MainActivity.java
+++ test_app_1/app/src/main/java/com/example/brandon/test_app_1/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.brandon.test_app_1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +11,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        switch (item.getItemId()) {
+//            case R.id.action_settings:
+//                // User chose the "Settings" item, show the app settings UI...
+//                return true;
+//
+//            case R.id.action_favorite:
+//                // User chose the "Favorite" action, mark the current item
+//                // as a favorite...
+//                return true;
+//
+//            default:
+//                // If we got here, the user's action was not recognized.
+//                // Invoke the superclass to handle it.
+//                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
+
+
 }
+
+
