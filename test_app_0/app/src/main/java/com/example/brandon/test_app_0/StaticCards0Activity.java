9ea2b7fc15ddc6bc435f4e63b110b12ece40e5d6
Brandon Valley <bavvh8@mst.edu>
2018-09-01 22:13:58 -0500

    made card that fills width and can control height

--- test_app_0/app/src/main/java/com/example/brandon/test_app_0/StaticCards0Activity.java
+++ test_app_0/app/src/main/java/com/example/brandon/test_app_0/StaticCards0Activity.java
@@ -1,8 +1,14 @@
 package com.example.brandon.test_app_0;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.widget.LinearLayout;
+
+import static java.sql.Types.NULL;
 
 public class StaticCards0Activity extends AppCompatActivity {
 
@@ -11,5 +17,64 @@ public class StaticCards0Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_static_cards_0);
         Intent intent = getIntent();
+
+
+        //define layout
+        LinearLayout container = (LinearLayout)findViewById(R.id.linear_layout_0); // have to put this in .xml manually
+
+        // Get the application context
+        Context mContext = getApplicationContext();
+
+
+
+
+
+
+        //add top blank card
+        // Initialize a new CardView
+        CardView card = new CardView(mContext);
+
+        // Set the CardView layoutParams
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+
+        int margin = 10;
+        params.topMargin   = margin;
+        params.rightMargin = margin;
+        params.leftMargin  = margin;
+        params.height = 100;
+
+
+        card.setLayoutParams(params);
+
+        card.setMinimumHeight(40);
+
+        // Set CardView corner radius
+        card.setRadius(9);
+
+        card.setMinimumWidth(container.getWidth());
+
+        // Set cardView content padding
+        card.setContentPadding(100, 30, 30, 30);
+
+        // Set a background color for CardView
+        card.setCardBackgroundColor(Color.parseColor("RED"));
+
+        // Set the CardView maximum elevation
+        card.setMaxCardElevation(15);
+
+        // Set CardView elevation
+        card.setCardElevation(9);
+
+        // Finally, add the CardView in root layout
+        container.addView(card);
+
+
+
+
+
+
     }
 }
