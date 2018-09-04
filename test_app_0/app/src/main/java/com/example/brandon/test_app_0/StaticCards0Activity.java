404f044c654a8023816383c72c9d3e2effa6ee68
Brandon Valley <bavvh8@mst.edu>
2018-09-04 18:52:25 -0500

    added green inside of blue card

--- test_app_0/app/src/main/java/com/example/brandon/test_app_0/StaticCards0Activity.java
+++ test_app_0/app/src/main/java/com/example/brandon/test_app_0/StaticCards0Activity.java
@@ -25,7 +25,7 @@ public class StaticCards0Activity extends AppCompatActivity {
         Intent intent = getIntent();
 
 
-        //define layout
+        //define linear layout for activity
         LinearLayout container = (LinearLayout)findViewById(R.id.linear_layout_0); // have to put this in .xml manually
 
         // Get the application context
@@ -71,35 +71,15 @@ public class StaticCards0Activity extends AppCompatActivity {
         card.setCardElevation(9);
 
 
-//        // Set the overflow button layoutParams
-//        LinearLayout.LayoutParams overflowBtnParams = card.getLayoutParams(
-////                cardv.LayoutParams.MATCH_PARENT,
-////                LinearLayout.LayoutParams.WRAP_CONTENT
-//        );
 
-//        RelativeLayout simpleRelativeLayout;
-//        simpleRelativeLayout.setid = simpleRelativeLayout;
-//        simpleRelativeLayout = (RelativeLayout) ;findViewById(R.id.simpleRelativeLayout);
-
-//        RelativeLayout.LayoutParams cardRelativeLayout =
-//                new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT,
-//                        RelativeLayout.LayoutParams.MATCH_PARENT);
-
+        //relative layout for inside of card
         RelativeLayout cardRelativeLayout = new RelativeLayout(getApplicationContext());
 
         //define relative layout button
         RelativeLayout.LayoutParams buttonParams =
                 new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT,
                                                 RelativeLayout.LayoutParams.MATCH_PARENT);
-
         buttonParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, RelativeLayout.TRUE);
-
-//        buttonParams.topMargin   = margin;
-//        buttonParams.rightMargin = card.getWidth();
-//        buttonParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, RelativeLayout.TRUE);
-//        buttonParams.lay
-        //buttonParams.leftMargin  = margin;
-        //cardViewParams.height = 200;
 
         ImageButton overflowBtn = new ImageButton(this);
         overflowBtn.setImageResource(R.drawable.baseline_more_vert_black_18dp);
@@ -123,8 +103,6 @@ public class StaticCards0Activity extends AppCompatActivity {
         tv.setTextColor(Color.BLACK);
         cardRelativeLayout.addView(tv);
 
-//        card.addView(tv);
-//        card.addView(overflowBtn);
         card.addView(cardRelativeLayout);
 
         // Finally, add the CardView in root layout
@@ -132,8 +110,70 @@ public class StaticCards0Activity extends AppCompatActivity {
 
 
 
+        // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ 1st card ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
 
 
+        //add big card
+        CardView bigCard = new CardView(mContext);
+
+        // Set the CardView layoutParams
+        LinearLayout.LayoutParams bigCardViewParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+
+//        int margin = 10;
+        bigCardViewParams.topMargin   = margin;
+        bigCardViewParams.rightMargin = margin;
+        bigCardViewParams.leftMargin  = margin;
+        bigCardViewParams.height = 400;  //change this to something about min hieght so it fits contents????
+
+
+        bigCard.setLayoutParams(bigCardViewParams);
+
+        bigCard.setRadius(9);
+        bigCard.setContentPadding(100, 30, 30, 30);
+        bigCard.setCardBackgroundColor(Color.parseColor("BLUE"));
+        bigCard.setMaxCardElevation(15);
+        bigCard.setCardElevation(9);
+
+
+        // Set the linear layout for inside the big card
+        LinearLayout.LayoutParams bigCardViewlayout = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+
+
+
+        //make 1st small card (green)
+        CardView greenCard = new CardView(mContext);
+
+        // Set the CardView layoutParams
+        LinearLayout.LayoutParams greenCardParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+
+//        int margin = 10;
+        greenCardParams.topMargin   = margin;
+        greenCardParams.rightMargin = margin;
+        greenCardParams.leftMargin  = margin;
+        greenCardParams.height = 200;  //change this to something about min hieght so it fits contents????
+
+
+        greenCard.setLayoutParams(greenCardParams);
+
+        greenCard.setRadius(9);
+        greenCard.setContentPadding(100, 30, 30, 30);
+        greenCard.setCardBackgroundColor(Color.parseColor("GREEN"));
+        greenCard.setMaxCardElevation(15);
+        greenCard.setCardElevation(9);
+
+        bigCard.addView(greenCard);
+
+
+        container.addView(bigCard);
+//        bigCard.addView(card);
     }
 }
