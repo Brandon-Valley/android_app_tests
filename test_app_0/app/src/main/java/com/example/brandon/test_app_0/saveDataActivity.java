2bcbbbd912f39b4a2525217da36943eda7992400
Brandon Valley <bavvh8@mst.edu>
2018-12-21 17:30:41 -0600

    got floating action button working with back button

--- test_app_0/app/src/main/java/com/example/brandon/test_app_0/saveDataActivity.java
+++ test_app_0/app/src/main/java/com/example/brandon/test_app_0/saveDataActivity.java
@@ -1,7 +1,10 @@
 package com.example.brandon.test_app_0;
 
+import android.content.Intent;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class saveDataActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class saveDataActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_save_data);
+    }
+
+    /** Called when the user taps the go to card activity button */
+    public void goToAddCardActivity(View view) {
+        Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
+//        Intent intent = new Intent(this, StaticCards0Activity.class);
+//        startActivity(intent);
     }
 }
